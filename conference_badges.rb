e9d16ef9bfee644d4de0656c157496dfa04c641c
Jered Sweeney <sweeneyjered@gmail.com>
2017-11-05 16:00:15 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,5 @@
 # Write your code here.
 def batch_badge_creator(array)
-  new_array = array.collect { | s | "Hello, my name is #{s}"}
+  new_array = array.collect { | s | "Hello, my name is #{s}."}
   new_array
 end
