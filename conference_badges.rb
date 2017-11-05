fe8e4c0455785b419d96ed3ae3c57fbcafa14933
Jered Sweeney <sweeneyjered@gmail.com>
2017-11-05 16:37:12 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,6 +10,6 @@ end
 
 
 def assign_rooms(array)
-  new_array = array.each_with_index.collect { | s, i | "Hello #{s}, You'll be assigned to room #{i + 1}!"}
+  new_array = array.each_with_index.collect { | s, i | "Hello #{s}! You'll be assigned to room #{i + 1}!"}
   new_array
 end
