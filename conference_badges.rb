f7bbebe7f0b1f7028f4226ac1dd83028938488bc
Jered Sweeney <sweeneyjered@gmail.com>
2017-11-05 17:00:29 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,3 +13,7 @@ def assign_rooms(array)
   new_array = array.each_with_index.collect { | s, i | "Hello #{s}! You'll be assigned to room #{i + 1}!"}
   new_array
 end
+
+def printer
+  batch_badge_creator.each {|phrase| puts "#{phrase}"}
+  assign_rooms.each {|phrase| puts "#{phrase}"}
